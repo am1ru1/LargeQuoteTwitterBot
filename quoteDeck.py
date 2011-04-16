c6b96c50ca86b2f3f0f0918ef7bbc7e9fd78c017
Mike Naylor <u0959418@unimail.hud.ac.uk>
2011-04-16 20:59:10 +0000

    Improved multi-tweet quote facility, now uses recursive calls to be more tweet efficient

--- quoteDeck.py
+++ quoteDeck.py
@@ -8,33 +8,28 @@ CONSUMER_SECRET = 'Your consumer secret key here'
 ACCESS_KEY = 'Your access key here'
 ACCESS_SECRET = 'Your access secret key here'
 MAX_TWEET_LENGTH = 140
-FILE_TO_USE = "your quotes file here e.g quotes.txt"
+FILE_TO_USE = "quotesfile.txt"
+tweetArray = []
 
 def randomLine(afile):
 	lines = open(afile).readlines()
-	return lines[random.randint(0, len(lines)-1)]
+	return lines[random.randint(0, (len(lines))-1)]
 
 def makeArray(astring):
 	try:
 		astring.index("\n")
 		astring = sanatiseString(astring)
 	except ValueError:
-		print 'No return character found, string is fine as is.'
+		print "No return character found, string is fine as is."
+	addToArray(astring)
 	
-	if len(astring)%MAX_TWEET_LENGTH == 0:
-		noOfTweets = len(astring)/MAX_TWEET_LENGTH
-	else:
-		noOfTweets = (len(astring)/MAX_TWEET_LENGTH)+1
+def addToArray(astring):
 
-	theArray = []
-	while noOfTweets > 0:
-		if noOfTweets == 1:
-			theArray.append(astring)
-		else:
-			theArray.append(astring[:astring[:MAX_TWEET_LENGTH-2].rindex(" ")]+'...')
-			astring = astring[astring[:MAX_TWEET_LENGTH-2].rindex(" "):]
-		noOfTweets -= 1
-	return theArray
+	if len(astring) <= 140:
+		tweetArray.append(astring)
+	else:
+		tweetArray.append(astring[:astring[:MAX_TWEET_LENGTH-3].rindex(' ')]+'...')
+		addToArray(astring[astring[:MAX_TWEET_LENGTH-3].rindex(' ')+1:len(astring)])
 
 def sanatiseString(astring):
 	sanatisedString = astring[:astring.index("\n")]
@@ -44,7 +39,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 quoteString = randomLine(FILE_TO_USE)
-tweetArray = makeArray(quoteString)
+makeArray(quoteString)
 
 for tweet in reversed(tweetArray):
 	api.update_status(tweet)
