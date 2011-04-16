4850ffdac609a1b4d624269dcc967b036c8faa81
Mike Naylor <u0959418@unimail.hud.ac.uk>
2011-04-15 20:48:19 -0700

    First commit of bot code

---
+++ botSay.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+ 
+import sys
+import tweepy
+import random
+
+CONSUMER_KEY = 'Your consumer key here'
+CONSUMER_SECRET = 'Your consumer secret key here'
+ACCESS_KEY = 'Your access key here'
+ACCESS_SECRET = 'Your access secret key here'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+api.update_status(sys.argv[1])
---
+++ quoteDeck.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import tweepy
+import random
+
+CONSUMER_KEY = 'Your consumer key here'
+CONSUMER_SECRET = 'Your consumer secret key here'
+ACCESS_KEY = 'Your access key here'
+ACCESS_SECRET = 'Your access secret key here'
+MAX_TWEET_LENGTH = 137
+FILE_TO_USE = "your quotes file here e.g quotes.txt"
+
+def randomLine(afile):
+	lines = open(afile).readlines()
+	return lines[random.randint(0, len(lines))]
+
+def makeArray(astring):
+	noOfTweets = (len(astring)/MAX_TWEET_LENGTH)+1
+	theArray = []
+	while noOfTweets > 0:
+		if noOfTweets == 1:
+			theArray.append(astring)
+		else:
+			theArray.append(astring[0:astring[0:137].rindex(" ")]+'...')
+			astring = astring[astring[0:137].rindex(" "):]
+		noOfTweets -= 1
+	return theArray
+
+	
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+quoteString = randomLine(FILE_TO_USE)
+tweetArray = makeArray(quoteString)
+
+for tweet in reversed(tweetArray):
+	api.update_status(tweet)
